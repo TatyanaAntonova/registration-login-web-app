9d17c332053401c0e203ce8940c722152ad9eaed
Your <tatyanaantonova@gmail.com>
2022-07-17 16:24:37 +0300

    add project

---
+++ src/main/java/com/example/registrationloginwebapp/RegistrationLoginWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registrationloginwebapp/RegistrationLoginWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrationloginwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
