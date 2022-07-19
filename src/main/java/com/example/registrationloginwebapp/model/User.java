8d22973dbe3609bda550249187cfdd56d141c37a
Your <tatyanaantonova@gmail.com>
2022-07-19 12:05:11 +0300

    add uniqueConstraints

--- src/main/java/com/example/registrationloginwebapp/model/User.java
+++ src/main/java/com/example/registrationloginwebapp/model/User.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
